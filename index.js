4f106ec86e01917750778bebb3ec27e79836da0e
Shin <jetaime1201@gmail.com>
2021-02-25 10:56:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const shin = express()
+
+const port = process.env.PORT || 3000
+
+shin.get('/', (req, res) => {
+  res.type('text/plain')
+  res.send('Meadowlark Travel')
+})
+  
+shin.get('/about', (req, res) => {
+  res.type('text/plain')
+  res.send('About Meadowlark Travel')
+})
+
+// custom 404 page
+shin.use((req, res) => {
+  res.type('text/plain')
+  res.status(404)
+  res.send('404 - Not Found')
+})
+
+// custom 500 page
+shin.use((err, req, res, next) => {
+  console.error(err.message)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Server Error')
+})
+
+shin.listen(port, () => console.log(
+  `Express started on http://localhost:${port}; ` +
+  `press Ctrl-C to terminate.`))
